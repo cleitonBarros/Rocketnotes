26ce021f2e30be95cf8c8cfe66b2d77deb6b479d
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-09 01:22:11 -0300

    chore: prettier and eslint

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,21 @@
+import React from "react";
+
 import * as S from "./styled";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   type?: "button" | "submit";
   isLoading?: boolean;
 }
 
-export function Button({ 
-  title, 
-  type, 
-  isLoading = false, 
-  ...props 
+export function Button({
+  title,
+  type,
+  isLoading = false,
+  ...props
 }: ButtonProps) {
-  
   return (
-    <S.Container 
-      type={type} 
-      disabled={isLoading} 
-      {...props}
-    >
+    <S.Container type={type} disabled={isLoading} {...props}>
       {isLoading ? "Carregando..." : title}
     </S.Container>
   );
