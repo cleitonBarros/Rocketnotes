2e88ca6a8b4c514d206d51a6b0a9d1f7e4901f3d
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-12 21:34:00 -0300

    chore: installed react-router-dom

--- src/pages/New/styled.ts
+++ src/pages/New/styled.ts
@@ -5,7 +5,7 @@ export const Container = styled.div`
   height: 100vh;
 
   display: grid;
-  grid-template-rows: 6.56rem auto;
+  grid-template-rows: auto;
   grid-template-areas:
     "header"
     "content";
